d68f82ead75b45afc766296b4e35b33106889f59
Júnior Medeiros <98979459+Medeiros000@users.noreply.github.com>
2025-11-06 01:29:44 -0300

    Refactor package.json and seed.ts: streamline Prisma commands and update dependencies

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,4 @@
-import { PrismaClient } from "@prisma/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
-
-const prisma = new PrismaClient().$extends(withAccelerate());
+import prisma from '../lib/prisma';
 
 const userData = [
 	{
